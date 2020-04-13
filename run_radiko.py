013017a1d045045e1cc6a4690ab42aa23fe958a2
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-04-13 17:57:28 +0900

    add : runner

--- run_radiko.py
+++ run_radiko.py
@@ -1,6 +1,43 @@
-from libradiko import *
+import argparse
+from datetime import datetime, timedelta, timezone
+import logging
+from record import record
+from post_slack import post_slack
+
+def _get_args():
+    parser = argparse.ArgumentParser(description='record radiko')
+    parser.add_argument('station',
+     type=str,
+     help='radiko station')
+    parser.add_argument('program',
+     type=str,
+     help='radiko program name')
+    parser.add_argument('recordtime',
+     type=int,
+     help='recording time.(unit:miniutes)')
+    parser.add_argument('-u', '--uploadgcloud',
+     type=bool,
+     help='upload recorded file to gcloud storage')
+    args = parser.parse_args()
+    return args.station, args.program, args.recordtime, args.uploadgcloud
 
 
 if __name__ == "__main__":
-    print(Authorization().get_auththenticated_headers())
-    print(radikotoday().show())
+    # ログ設定をする
+    logging.basicConfig(filename=f'record_radiko.log', level=logging.DEBUG)
+    # 実行時パラメータを取得する
+    station, program, rtime, uploads = _get_args()
+
+    JST = timezone(timedelta(hours=+9), 'JST')
+    current_time = datetime.now(tz=JST).strftime("%Y%m%d_%H%M")
+    logging.debug(f'current time: {current_time}, \
+        station: {station}, \
+        program name: {program}, \
+        recording time: {rtime}, \
+        uploads: {uploads}')
+    # 録音保存先を用意する
+    outfilename = f'./tmp/{current_time}_{station}_{program}.aac'
+    logging.debug(f'outfilename:{outfilename}')
+    # 録音
+    record(station, program, rtime, outfilename)
+    post_slack("自動ポスト","Done")
