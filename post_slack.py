418a6f4bd635554a9ac70fc1b464a47329bb74b8
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-04-13 20:11:08 +0900

    add : post slack

---
+++ post_slack.py
@@ -0,0 +1,14 @@
+import requests
+import json
+from token_file import post_url
+
+
+def post_slack(name, text):
+    requests.post(
+        post_url,
+        data=json.dumps(
+            {"text": text,
+             "username": name,
+             "icon_emoji": ":python:"}))
+    
+# post_slack("自動ポスト","Done")
